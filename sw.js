05007fa1a5757d3632042025b4a285eb4ac545cd
26mx <mxmx267@outlook.com>
2025-07-30 20:20:36 +0800

    Site updated: 2025-07-30 20:20:35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1753876072561";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1753878017367";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
